6b0fcdee3fc22b726de709ebd0264c16390ea4ca
sbx_user1051 <sbx_user1051@169.254.155.125>
2022-07-17 10:43:04 +0000

    Sun Jul 17 10:43:04 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,10 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    output_string = "Hello, World!"
+def hello(friend_name: str):
+
+    if not isinstance(friend_name, str):
+        raise TypeError("friend_name must be a string")
+
+    output_string = f"Hello, {friend_name}!"
     return output_string
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,5 +1,13 @@
 from lib.solutions.HLO.hello_solution import hello
 
 class TestSum():
-    def test_hello_returns_expected_string_with_no_name_provided(self):
-        assert hello("") == "Hello, World!"
+    def test_hello_returns_expected_string_with_name(self):
+        assert hello("test") == "Hello, test!"
+    
+    def test_invalid_name_type_raises_typeerror(self):
+        raised = False
+        try:
+            hello(123545)
+        except TypeError:
+            raised = True
+        assert raised is True
