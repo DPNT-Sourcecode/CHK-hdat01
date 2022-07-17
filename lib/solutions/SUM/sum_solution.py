7d9140f2831a96e28220eb1b030c95c416e6ecca
sbx_user1051 <sbx_user1051@169.254.155.125>
2022-07-17 10:19:28 +0000

    Sun Jul 17 10:19:28 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,20 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+MIN_ALLOWED_VALUE = 0
+MAX_ALLOWED_VALUE = 100
+
+
+def _in_acceptable_range(value) -> bool:
+    """
+    Returns true if the given value is within acceptable range.
+    """
+    return value >= MIN_ALLOWED_VALUE and value <= MAX_ALLOWED_VALUE
+
+
 def compute(x, y):
+
+    if not isinstance(x, int) or not isinstance(y, int):
+        raise ValueError("Only integers are allowed in compute function.")
+    
     raise NotImplementedError()
+
