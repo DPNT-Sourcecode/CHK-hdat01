198208a82317f116feb85f8b7252d0ac2db23bd1
sbx_user1051 <sbx_user1051@169.254.155.125>
2022-07-17 10:25:28 +0000

    Sun Jul 17 10:25:28 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,19 @@
-from solutions.SUM import sum_solution
+from lib.solutions.SUM import sum_solution
 
 
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+    
+    def test_x_is_not_integer_raises_typeerror(self):
+        pass
+    
+    def test_y_is_no_ingeger_raises_typeerror(self):
+        pass
+
+    def test_x_is_less_than_min_allowed_value_raises_valueerror(self):
+        pass
+
+    def test_y_is_less_than_min_allowed_value_raises_valueerror(self):
+        pass
+
