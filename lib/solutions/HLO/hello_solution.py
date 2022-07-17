2523134c0e96877de472a97a6f14042bc720f0d1
sbx_user1051 <sbx_user1051@169.254.155.125>
2022-07-17 10:40:31 +0000

    Sun Jul 17 10:40:31 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,5 +3,5 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    output_string = "Hello World"
+    output_string = "Hello, World!"
     return output_string
--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -2,4 +2,4 @@ from lib.solutions.HLO.hello_solution import hello
 
 class TestSum():
     def test_hello_returns_expected_string_with_no_name_provided(self):
-        assert hello("") == "Hello World"
+        assert hello("") == "Hello, World!"
