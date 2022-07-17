bd3bd3ecea46c4b0d582a53b2533ed2c122b4001
sbx_user1051 <sbx_user1051@169.254.155.125>
2022-07-17 10:31:28 +0000

    Sun Jul 17 10:31:28 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,24 +1,25 @@
 import pytest
-from lib.solutions.SUM import MAX_ALLOWED_VALUE, MIN_ALLOWED_VALUE, sum_solution
+from lib.solutions.SUM.sum_solution import compute, MAX_ALLOWED_VALUE, MIN_ALLOWED_VALUE
 
 class TestSum():
     def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
+        assert compute(1, 2) == 3
     
     def test_x_is_not_integer_raises_typeerror(self):
         with pytest.raises(TypeError, match="Only integers are allowed in compute function."):
-            sum_solution.compute(14.7, 1)
+            compute(14.7, 1)
     
     def test_y_is_no_ingeger_raises_typeerror(self):
         with pytest.raises(TypeError, match="Only integers are allowed in compute function."):
-            sum_solution.compute(1, 14.7)
+            compute(1, 14.7)
 
     def test_x_is_less_than_min_allowed_value_raises_valueerror(self):
         with pytest.raises(TypeError, match=f"param x must be between {MIN_ALLOWED_VALUE} and {MAX_ALLOWED_VALUE} inclusive."):
-            sum_solution.compute(-1, 14)
+            compute(-1, 14)
 
     def test_y_is_less_than_min_allowed_value_raises_valueerror(self):
         with pytest.raises(TypeError, match=f"param y must be between {MIN_ALLOWED_VALUE} and {MAX_ALLOWED_VALUE} inclusive."):
-            sum_solution.compute(14, -1)
+            compute(14, -1)
+
 
 
